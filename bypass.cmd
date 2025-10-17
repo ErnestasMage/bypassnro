45b01f126a7d77b6b6b20f374660db06b0b55005
Ernestas Mage <68244097+ErnestasMage@users.noreply.github.com>
2025-10-18 00:24:28 +0300

    Update bypass.cmd

--- bypass.cmd
+++ bypass.cmd
@@ -1,2 +1,2 @@
-curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/ChrisTitusTech/bypassnro/refs/heads/main/unattend.xml
+curl -L -o C:\Windows\Panther\unattend.xml https://github.com/ErnestasMage/bypassnro/blob/5841529c58d23d14a9356414a476f2586588f48c/unattend.xml
 %WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
